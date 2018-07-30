bc0ea48ce0470b5964d28a21448846a2b88b6e4d
aalaksana <mail.laksana@gmail.com>
2018-07-30 19:59:17 +0700

    atasi bug dashboard
    query menghasilkan lebih dari 1 value. dibuat limit 1

--- la1/app/Http/Controllers/LA/DashboardController.php
+++ la1/app/Http/Controllers/LA/DashboardController.php
@@ -44,9 +44,9 @@ class DashboardController extends Controller
         $datas = DB::select("Select * from projects");
 
         $proyek = DB::select("select p.nama, p.status, 
-            ifnull((select t.nama_pj from teams t where t.peran='Proses Bisnis' and p.id=t.project_id ),0) as id_probis, 
-            ifnull((select t.nama_pj from teams t where t.peran='Pengembang SI' and p.id=t.project_id ),0) as id_si, 
-            ifnull((select t.nama_pj from teams t where t.peran='Tim QA' and p.id=t.project_id ),0) as id_qa from projects p where p.id=".$id);
+            ifnull((select t.nama_pj from teams t where t.peran='Proses Bisnis' and p.id=t.project_id limit 1),0) as id_probis, 
+            ifnull((select t.nama_pj from teams t where t.peran='Pengembang SI' and p.id=t.project_id limit 1),0) as id_si, 
+            ifnull((select t.nama_pj from teams t where t.peran='Tim QA' and p.id=t.project_id limit 1),0) as id_qa from projects p where p.id=".$id);
         $probis = DB::select("select u.name, u.nip, d.nama_instansi, d.es1,d.es2 from users u, departments d
             where u.instansi=d.id and u.id=".$proyek[0]->id_probis);
         $si = DB::select("select u.name, u.nip, d.nama_instansi, d.es1,d.es2 from users u, departments d
